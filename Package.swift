6a151e9a742920c2a6feeb8fd90c4def3f75d1ef
Afnan Mirza <afnanmirza1999@gmail.com>
2020-10-15 02:34:21 -0700

    Replacing `EasyNotificationBadge` dependency source to point at the proper url.

--- Package.swift
+++ Package.swift
@@ -10,13 +10,9 @@ let package = Package(
             targets: ["AZTabBarController"]),
     ],
     dependencies: [
-        // Replace the code from line 17 to 19 with the code from line 14 to line 16 after `https://github.com/Minitour/EasyNotificationBadge/pull/16` is merged.
-        //        .package(name: "EasyNotificationBadge",
-        //                 url: "https://github.com/Minitour/EasyNotificationBadge.git",
-        //                 from: "1.4.2"),
         .package(name: "EasyNotificationBadge",
-                 url: "https://github.com/afnanm1999/EasyNotificationBadge.git",
-                 .branch("SPM-Integration"))
+                 url: "https://github.com/Minitour/EasyNotificationBadge.git",
+                 from: "1.2.0"),
     ],
     targets: [
         .target(
